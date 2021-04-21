161dcb20efd1ca2dc8bae8f74ad50d84e5693aeb
Jervin descrates kontessa <71800787+Jervinkontessa@users.noreply.github.com>
2021-04-21 12:00:22 +0800

    add

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <style>
+      .center{
+         margin-left: auto;
+         margin-right: auto;
+         margin-top: 6px;
+
+      }
+      .span{
+          width:100%;
+          text-align:center;
+      }
+    </style>
+    <title></title>
+  </head>
+
+  <body>
+    <div class="span">
+      <b>
+      <span>Biodataku</span>
+      </b>
+    </div>
+    <table border="2" cellspacing="0" cellpadding="5" class="center">
+
+
+    <tr>
+      <th>Nama</th>
+      <td>
+        <?=
+          "Jervin Descrates Kontessa ";
+         ?>
+      </td>
+    </tr>
+    <tr>
+      <th>NIM</th>
+      <td>
+        <?=
+          "51018013 ";
+         ?>
+      </td>
+    </tr>
+    <tr>
+      <th>Jurusan</th>
+      <td>
+        <?=
+          "Sistem informasi";
+         ?>
+      </td>
+    </tr>
+  </body>
+</html>
